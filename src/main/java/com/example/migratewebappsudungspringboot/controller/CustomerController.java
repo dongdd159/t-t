1aa883f6270e8ef5acf2eb557825c0852173b431
admin <dongdd159@gmail.com>
2021-08-06 10:12:56 +0700

    fsfs

--- src/main/java/com/example/migratewebappsudungspringboot/controller/CustomerController.java
+++ src/main/java/com/example/migratewebappsudungspringboot/controller/CustomerController.java
@@ -64,4 +64,7 @@ public class CustomerController {
     private Page<Customer> swwwwwwwwwwwwww(Optional<String> s, Pageable pageInfo) {
         return customerService.search(s.get(), pageInfo);
     }
+    private Page<Customer> ddddddddddddddddddddddddd(Optional<String> s, Pageable pageInfo) {
+        return customerService.search1(s.get(), pageInfo);
+    }
 }
